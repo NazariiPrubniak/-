8e0d710c96acf704040314c3ddf758f31b865650
NazariiPrubniak <72340709+NazariiPrubniak@users.noreply.github.com>
2020-11-26 12:59:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-messange=str(input("Enter word: "))
-new_messange=""
-for i in messange[::-1]:
-    new_messange+=i
-print("new text: ",new_messange)
-input("\n\nEnter.")
+from string import punctuation
 
+
+A_or_B = ''
+
+while True:
+    print("A) Sort words with alphabet\nB) Get numbers of all letters in text")
+    a_or_b = input("Choose A or B: ")
+
+    if a_or_b in ['A', 'B']:
+        break
+    else:
+        print('Try Again, Please')
+
+
+text = input("Write some text here")
+
+if a_or_b == 'A':
+    words = text.split()
+    words = set([word for word in words if len(word) > 2])
+    words = list(words)
+    words.sort(reverse=0)
+    print('Words sort with alphabet:', end="\n\t")
+    print(', '.join(words))
+else:
+    chars = {}
+    for char in text:
+        if char not in punctuation and char != " ":
+            chars[char] = chars[char]+1 if char in chars else 1
+    print("Number of all letters in text:")
+    for i in chars.items():
+        print(f"\t{i[0]}: {i[1]}")
